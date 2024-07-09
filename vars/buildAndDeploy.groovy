5f608ba06c9641b87e8cb6f4f1ddf69ee984ee6b
Alex <alexb853@gmail.com>
2024-07-09 12:32:11 +0300

    add shared library

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,63 +1,60 @@
 // vars/buildAndDeploy.groovy
 def call() {
+    stage('Pull Nginx Image') {
+        steps {
+            script {
+                pipelineUtils.pullNginxImage()
+            }
+        }
+    }
 
-    stages {
-        stage('Pull Nginx Image') {
-            steps {
-                script {
-                    pipelineUtils.pullNginxImage()
+    stage('Build Polybot Image') {
+        steps {
+            script {
+                pipelineUtils.buildPolybotImage(env.POLYBOT_IMG_NAME)
+            }
+        }
+    }
+
+    stage('Build Docker Image') {
+        steps {
+            script {
+                pipelineUtils.buildDockerImage(env.DOCKER_COMPOSE_FILE)
+            }
+        }
+    }
+
+    stage('Snyk Container Test') {
+        steps {
+            script {
+                pipelineUtils.snykContainerTest(env.APP_IMAGE_NAME, env.SNYK_TOKEN)
+            }
+        }
+    }
+
+    stage('Tag and Push Images') {
+        steps {
+            script {
+                withCredentials([
+                    usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASS')
+                ]) {
+                    pipelineUtils.tagAndPushImages(env.APP_IMAGE_NAME, env.WEB_IMAGE_NAME, env.IMAGE_TAG, env.DOCKER_USERNAME, env.DOCKER_PASS)
                 }
             }
         }
+    }
 
-        stage('Build Polybot Image') {
-            steps {
-                script {
-                    pipelineUtils.buildPolybotImage(env.POLYBOT_IMG_NAME)
-                }
+    stage('Trigger Deploy') {
+        steps {
+            script {
+                pipelineUtils.triggerDeploy("alexb853/${env.POLYBOT_IMG_NAME}")
             }
         }
+    }
 
-        stage('Build Docker Image') {
-            steps {
-                script {
-                    pipelineUtils.buildDockerImage(env.DOCKER_COMPOSE_FILE)
-                }
-            }
-        }
-
-        stage('Snyk Container Test') {
-            steps {
-                script {
-                    pipelineUtils.snykContainerTest(env.APP_IMAGE_NAME, env.SNYK_TOKEN)
-                }
-            }
-        }
-
-        stage('Tag and Push Images') {
-            steps {
-                script {
-                    withCredentials([
-                        usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASS')
-                    ]) {
-                        pipelineUtils.tagAndPushImages(env.APP_IMAGE_NAME, env.WEB_IMAGE_NAME, env.IMAGE_TAG, env.DOCKER_USERNAME, env.DOCKER_PASS)
-                    }
-                }
-            }
-        }
-
-        stage('Trigger Deploy') {
-            steps {
-                script {
-                    pipelineUtils.triggerDeploy("alexb853/${env.POLYBOT_IMG_NAME}")
-                }
-            }
-        }
-
-        stage('Sleep') {
-            steps {
-                sleep 20
-            }
+    stage('Sleep') {
+        steps {
+            sleep 20
         }
     }
 
