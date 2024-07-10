53ef273846aa66f14034e6363ea7836f0bc5b5e1
Alex <alexb853@gmail.com>
2024-07-10 20:19:08 +0300

    add shared library

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,68 +1,9 @@
 // vars/buildAndDeploy.groovy
 def call() {
-  node {
-    stage('Pull Nginx Image') {
-        steps {
-            script {
-                pipelineUtils.pullNginxImage()
+   stage('Trigger Deploy') {
+            steps {
+               build job: 'BotDeploy', wait: false, parameters: [
+                string(name: 'IMAGE_URL', value: "alexb853/$POLYBOT_IMG_NAME")
+                ]
             }
-        }
-    }
-
-    stage('Build Polybot Image') {
-        steps {
-            script {
-                pipelineUtils.buildPolybotImage(env.POLYBOT_IMG_NAME)
-            }
-        }
-    }
-
-    stage('Build Docker Image') {
-        steps {
-            script {
-                pipelineUtils.buildDockerImage(env.DOCKER_COMPOSE_FILE)
-            }
-        }
-    }
-
-    stage('Snyk Container Test') {
-        steps {
-            script {
-                pipelineUtils.snykContainerTest(env.APP_IMAGE_NAME, env.SNYK_TOKEN)
-            }
-        }
-    }
-
-    stage('Tag and Push Images') {
-        steps {
-            script {
-                withCredentials([
-                    usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASS')
-                ]) {
-                    pipelineUtils.tagAndPushImages(env.APP_IMAGE_NAME, env.WEB_IMAGE_NAME, env.IMAGE_TAG, env.DOCKER_USERNAME, env.DOCKER_PASS)
-                }
-            }
-        }
-    }
-
-    stage('Trigger Deploy') {
-        steps {
-            script {
-                pipelineUtils.triggerDeploy("alexb853/${env.POLYBOT_IMG_NAME}")
-            }
-        }
-    }
-
-    stage('Sleep') {
-        steps {
-            sleep 20
-        }
-    }
-
-    post {
-        always {
-            cleanWs()
-        }
-    }
-  }
-}
+   }
